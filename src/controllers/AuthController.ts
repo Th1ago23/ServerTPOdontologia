aec7febf2a9ce0eed52cc80f6a556fbe31265267
Thiago Peixoto <thiago.peixots@gmail.com>
2025-06-11 23:46:04 -0300

    faz o l

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { createVerificationCode, verifyCode, generateVerificationCode, sendVerificationEmail, verifyEmail } from "../services/verificationService";
+import { generateVerificationCode, sendVerificationEmail, verifyEmail } from "../services/verificationService";
 
 const prisma = new PrismaClient();
 const saltRounds = 10;
